956789fd0d806ce114ce3e24a745243726aa0dad
StrongTyphoon <just5538@gmail.com>
2023-04-01 15:11:46 +0900

    2023-04-01

--- CodingTestStudy/14502연구소/14502.js
+++ CodingTestStudy/14502연구소/14502.js
@@ -1,2 +1,3 @@
 const fs = require('fs')
-const inp = fs.readFileSync('./14502연구소/input.txt').toString()
+const inp = fs.readFileSync('./14502연구소/input.txt').toString().trim().split('\n')
+const [N,M] = 
