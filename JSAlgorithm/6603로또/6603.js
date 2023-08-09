1d5f587bc59db3610e00bda1335cc31f45d96f42
TaePoong719 <just5538@gmail.com>
2023-08-09 21:29:59 +0900

    2023-08-09

---
+++ JSAlgorithm/6603로또/6603.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const inp = fs.readFileSync('./6603로또/input.txt').toString().trim().replaceAll('\r','').split('\n')
+
+
+let ansAry = []
+
+for(let i=0; i<inp.length-1; i++){
+  const [N, ...inpAry] = inp[i].split(' ').map(e=>+e)
+  // console.log(N, inpAry)
+  let ans = ""
+  const bfs = ( visited = [] ) => {
+    if(visited.length === 6){
+      ans += visited.join(" ")+ "\n"
+    }
+    
+    for(let j=0; j<N; j++){
+      if( visited.findIndex(val=>val===inpAry[j]) !== -1 ) continue
+      if( inpAry[j] < visited.slice(-1)[0] ) continue
+      const idx = visited.length
+      visited.push(inpAry[j])
+      bfs(visited)
+      visited.splice(idx,1)
+    }
+  } 
+
+  bfs()
+  ansAry.push(ans)
+}
+console.log(ansAry.join('\n'))
