49b8a5d0de20f4b09203e8f19b1e9b283717069c
TaePoong719 <just5538@gmail.com>
2023-08-30 23:16:30 +0900

    2023-08-30

--- JSAlgorithm/5648역원소정렬/5648why.js
+++ JSAlgorithm/5648역원소정렬/5648why.js
@@ -1,17 +1,20 @@
 const fs = require('fs')
-const inp = fs.readFileSync('./5648역원소정렬/input.txt').toString().replaceAll('\r','').split('\n').map(ary=>ary.trim())
-console.log(inp)
-const N = +inp[0].split(' ')[0]
-let inpAry = []
-for(let str of inp){
-  inpAry.push(str.split(' ').map(s=>{
-    let retStr = ""
-    for(let i=s.length-1 ; i>=0; i--){
-      retStr += s[i]
-    }
-    return +retStr
-  }))
-}
+const inp = fs.readFileSync('5648역원소정렬/input.txt').toString().replaceAll('\r','').trim().split('\n').map(ary=>ary.trim())
 
+let inpAry = []
+
+// 모든 수를 거꾸로 받기
+for(let str of inp){
+  str.split(' ').forEach(str=>{
+    let tempStr = ''
+    for(let i=str.length-1; i>=0; i--){
+      tempStr += str[i]
+    }
+    inpAry.push(+tempStr)
+  })
+}
+// 맨 첫번째 수를 slicing
 inpAry = inpAry.slice(1)
-console.log(inpAry)
+
+// 정렬 후 출려
+console.log(inpAry.sort(function(a,b){return a-b}).join('\n'))
